c5ba5d490d887d32ec3b3d7e50c262203ea0bea0
ahmed <ahmedzwawi46@gmail.com>
2025-02-23 08:41:58 +0100

    Modification produitstep

--- cypress/steps/product.steps.js
+++ cypress/steps/product.steps.js
@@ -1,24 +1,28 @@
+import loginPage from '../pages/login.page';
 import ProductsPage from '../pages/products.page';
-import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+import {Given, When, Then} from "cypress-cucumber-preprocessor/steps";
 let selectedProduct;
 
 Given('je suis sur {string}', function (url) {
   cy.visit(url);
 });
 
-When('je saisis le username {string}', function (username) {
-  cy.get('#user-name').type(username);
+When('j-e saisis le username {string}', function (username) {
+  // cy.get('#user-name').type(username);
+  loginPage.saisirUsername(username);
 });
 When('je saisi le password {string}', function (password) {
-  cy.get('#password').type(password);
+  loginPage.saisirPassword(password)
 });
 
-When('je clique sur le bouton {string}', function (buttonId) {
-  cy.get(`#${buttonId}`).click();
+When('je clique sur le bouton {string}', function () {
+  // cy.get(`#${buttonId}`).click();
+  loginPage.cliqueSurLoginButton();
+
 });
 
 Then('je suis redirigé au dashbord', function () {
-  cy.url().should('include', '/inventory.html');
+  cy.url().should('include', '/inventory.html'); 
 });
 
 Then('je verifie la liste des produits', function () {
@@ -27,7 +31,7 @@ Then('je verifie la liste des produits', function () {
 
 //Tri par nom AZ
 When('je trie les produits par nom', function () {
-  ProductsPage.selectionnerTriPar('az');
+  ProductsPage.selectionnerTriPar('az'); 
 });
 
 Then('je vérifie que les produits sont triés par nom', function () {
@@ -47,9 +51,7 @@ Then('je vérifie que les produits sont triés par prix', () => {
   ProductsPage.elements.productPrice().then(($prices) => {
     const productPrices = $prices.toArray().map((el) => parseFloat(el.innerText.replace('$', '')));
     const sortedProductPrices = [...productPrices].sort((a, b) => a - b);
-    // expect(productPrices).to.deep.equal(sortedProductPrices);
-    assert.deepEqual(productPrices, sortedProductPrices, 'Les produits ne sont pas triés par prix');
-
+    expect(productPrices).to.deep.equal(sortedProductPrices);
   });
 });
 
@@ -57,7 +59,7 @@ Then('je vérifie que les produits sont triés par prix', () => {
 When('je clique sur {string}', (s) => {
   ProductsPage.selectRandomProduct().then((randomProduct) => {
     selectedProduct = randomProduct; // Stocker le produit sélectionné
-    ProductsPage.recupererNomProduit(randomProduct).then((nomProduit) => {
+      ProductsPage.recupererNomProduit(randomProduct).then((nomProduit) => {
       // Formater le nom du produit pour correspondre à l'attribut data-test
       const nomProduitFormate = nomProduit.toLowerCase().replace(/\s+/g, '-');
       ProductsPage.ajouterProduitAuPanier(nomProduitFormate);
@@ -76,8 +78,7 @@ When('je supprime le produit', () => {
       const nomProduitFormate = nomProduit.toLowerCase().replace(/\s+/g, '-');
       ProductsPage.retirerProduitDuPanier(nomProduitFormate);
     });
-  });
-})
+  });})
 
 Then('le produit est bien supprimé du panier', () => {
   ProductsPage.verifierProduitSupprime();
